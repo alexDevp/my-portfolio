1e7ecd20421205da981fe3a6d12d3a7898f45fe0
Alexandre Pereira <cp.alexandrepereira@gmail.com>
2021-12-02 12:37:49 +0000

    Bug Fix
    Router not working 100%

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom";
 import {BrowserRouter as Router} from 'react-router-dom'
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import SimpleReactLightbox from 'simple-react-lightbox'
 
-ReactDOM.render(<Router><SimpleReactLightbox><App /></SimpleReactLightbox></Router>, document.getElementById("root"));
+ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
